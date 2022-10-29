dbf982964555f532d6f5d44945debff043f9a8bb
sobirismatov <name@uzcoder96@gmail.com>
2022-10-29 23:59:12 +0500

    a

--- String01.py
+++ String01.py
@@ -1,9 +1,10 @@
-def main(s):
+def main(a):
     """"Return an optional string of length eight
     Args:
         None
     Returns:
         str: return answer.
     """
-    if len(s)==8:
-       return s 
+    
+    len(a)==8
+    return a 
