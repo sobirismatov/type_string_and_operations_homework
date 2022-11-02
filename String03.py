852ee062fbcc640720c164bf18930117a99a58d5
sobirismatov <name@uzcoder96@gmail.com>
2022-11-02 16:15:15 +0500

    09

--- String03.py
+++ String03.py
@@ -1,4 +1,4 @@
-def main(a):
+def main():
     """
     "*" Return a string that consists of only this character and is five in length.
     Args:
@@ -8,5 +8,5 @@ def main(a):
     """
    
 
-    return  str(5*"*")
+    return  5*"*"
 
